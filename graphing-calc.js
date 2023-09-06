57cf4bb230eba5a6ddcc14847d8277f89ed90881
rdalton1124 <rebecca.dalton1124@gmail.com>
2023-09-06 19:30:36 -0400

    test

--- graphing-calc.js
+++ graphing-calc.js
@@ -1,8 +1,9 @@
+import csc from "./mytrig.js"; 
 let canvas; 
 let context; 
 
 let fn = function (x) {
-	return (x * x * .25) - 4;
+	return Math.sin(x);
 }
 function init() { 
 	canvas = document.getElementById("canvas"); 
@@ -10,18 +11,26 @@ function init() {
 	context.strokeStyle = 'black'; 
 	context.strokeRect(1, 1, canvas.width - 1, canvas.height - 1);  	
 	drawCoordPlane(); 
+	drawGraph(fn);
 }
 function drawCoordPlane() { 
+	event.preventDefault();
+	//clear any existing drawings. 
+	context.fillStyle = 'white'; 
+	context.fillRect(2, 2, canvas.width - 3, canvas.height - 3);
+	
+	//draw x and y axes. 
 	context.fillStyle = 'black'; 
 	let midX = canvas.width / 2; 
 	let midY = canvas.height / 2; 
-
 	context.fillRect(0, midY - 1, canvas.width, 3);
 	context.fillRect(midX, 0, 3, canvas.height); 
+	//label x and y axes
 	context.font = '30px sans-serif';
 	context.fillText("x", 0, midY + 20);
 	context.fillText("y", midX + 10, 20);
 
+	//draw grid for x [-15, 15] and y [-15, 15]
 	context.strokeStyle = 'gray'; 
 	context.beginPath(); 
 	let inc = canvas.width / 30; 
@@ -35,14 +44,14 @@ function drawCoordPlane() {
 		context.lineTo(canvas.width, i);
 	}
 	context.stroke();
-	drawGraph(fn); 
 }
-function convertWidth(x) { //given an X find its 
+
+function convertWidth(x) { //given an X find its location on canvas 
 	x = x * (canvas.width / 30); 
 	x += (canvas.width) / 2; 
 	return x;  
 }
-function convertHeight(y) {
+function convertHeight(y) { //given a y, find its location on canvas
 	y = y * (canvas.height / 30);
 	y += canvas.height / 2;
 	return y; 
@@ -52,6 +61,7 @@ function drawGraph(fun) {
 	context.beginPath() 
 	context.moveTo(-15, fn(-15)); 
 	for(let i = -15; i < 15; i += .1) {
+		//invert f(x) so that it displays correctly. 
 		context.lineTo(convertWidth(i), convertHeight(-fn(i)));
 	}
 	context.stroke();
---
+++ mytrig.js
@@ -0,0 +1,9 @@
+export function csc(x) {
+	return 1 / Math.sin(x);
+}
+export function sec(x) {
+	return 1 / Math.cos(x); 
+}
+export function cot(x) {
+	return 1 / Math.tan(x); 
+}
